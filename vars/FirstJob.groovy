45594478af049e1890608d752caf19929cfe56c6
Anil Kumar <anilk.vp@gmail.com>
2022-07-04 20:29:49 +0530

    thrird changes

--- vars/FirstJob.groovy
+++ vars/FirstJob.groovy
@@ -3,7 +3,7 @@
 def call(body){
     def config = [:]
     echo "Calling FirstJob.call() with parameter ${body}"
-    body.resolveStrategy = Closure.DELEGATE_FIRST
+    //body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
     echo "Calling FirstJob.call() printing config parameters ${body}"
