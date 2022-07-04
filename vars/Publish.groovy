efab0f61a93bcc4fc4460746b7521b46d4af0b1f
Anil Kumar <anilk.vp@gmail.com>
2022-07-04 22:15:09 +0530

    thrird changes

--- vars/Publish.groovy
+++ vars/Publish.groovy
@@ -3,8 +3,9 @@
 def call(Closure body){
     echo 'Calling Publish'
     node {
-        stage('Pre publish stage')
-        echo 'Inside publish node........................'
+        stage('Pre publish stage'){
+            echo 'Inside pre publish stage........................'
+        }
     }
     body()
 }
