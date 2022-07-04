50c786f862baf6e3b8499789f29bfd1685ef2ac4
Anil Kumar <anilk.vp@gmail.com>
2022-07-04 21:03:17 +0530

    thrird changes

--- vars/FirstJob.groovy
+++ vars/FirstJob.groovy
@@ -4,10 +4,13 @@ def call(body){
     def config = [:]
     echo "Calling FirstJob.call() with parameter ${body}"
     //body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    // body.delegate = config
+    // body()
     echo "Calling FirstJob.call() printing config parameters ${body}"
     node(config.nodeName){
         echo "Inside node ${config.nodeName}"
+        stage('Sub Stage') {
+            echo 'A stage inside FirstJob'
+        }
     }
 }
